e2fefaa2cb5f52ebcf9546830940cbe7e7e2195b
Is44cNascimento <isaacgamex7@outlook.com>
2025-04-09 19:44:30 -0300

    Completo: 09-04-2025

--- src/Contador.java
+++ src/Contador.java
@@ -12,15 +12,31 @@ public class Contador {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
-        }catch ( exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+  //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro!");
+
         }
 
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+
 
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo número " + i);
+        }
+
     }
+
+
+
+
+
 }
